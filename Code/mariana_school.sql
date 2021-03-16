04ca90140206c84b3824fed4176d91ad3f8e2976
Ouchen <ouchen_mohamed@outlook.com>
2021-03-16 09:22:47 +0100

    DATABASE

--- Code/mariana_school.sql
+++ Code/mariana_school.sql
@@ -1,67 +1,134 @@
-DROP TABLE IF EXISTS administrator;
-CREATE TABLE `administrator` (
-  `idA` int(11) NOT NULL AUTO_INCREMENT,
-  `idV` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idA`),
-  KEY `idV` (`idV`),
-  CONSTRAINT `administrator_ibfk_1` FOREIGN KEY (`idV`) REFERENCES `visitor` (`idV`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-DROP TABLE IF EXISTS marks;
-CREATE TABLE `marks` (
-  `idStu` int(11) NOT NULL,
-  `idSub` int(11) NOT NULL,
-  `mark` float DEFAULT NULL,
-  KEY `idStu` (`idStu`),
-  KEY `idSub` (`idSub`),
-  CONSTRAINT `marks_ibfk_1` FOREIGN KEY (`idStu`) REFERENCES `student` (`idStu`),
-  CONSTRAINT `marks_ibfk_2` FOREIGN KEY (`idSub`) REFERENCES `subjects` (`idSub`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-DROP TABLE IF EXISTS student;
-CREATE TABLE `student` (
-  `idStu` int(11) NOT NULL AUTO_INCREMENT,
-  `full_name` varchar(255) DEFAULT NULL,
-  `gender` varchar(255) DEFAULT NULL,
-  `age` int(11) DEFAULT NULL,
-  `adress` varchar(255) DEFAULT NULL,
-  `parent_phone` int(11) DEFAULT NULL,
-  `class` varchar(255) DEFAULT NULL,
-  `idV` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idStu`),
-  KEY `idV` (`idV`),
-  CONSTRAINT `student_ibfk_1` FOREIGN KEY (`idV`) REFERENCES `visitor` (`idV`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
-
-DROP TABLE IF EXISTS subjects;
-CREATE TABLE `subjects` (
-  `idSub` int(11) NOT NULL AUTO_INCREMENT,
-  `subject_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`idSub`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
-
-DROP TABLE IF EXISTS visitor;
-CREATE TABLE `visitor` (
-  `idV` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) DEFAULT NULL,
-  `login_password` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`idV`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
-
-
-
-
-
-INSERT INTO marks(idStu,idSub,mark) VALUES(1,1,12),(1,2,18),(2,1,14),(2,2,12),(3,1,15),(3,2,12);
-
-INSERT INTO student(idStu,full_name,gender,age,adress,parent_phone,class,idV) VALUES(1,'Hicham','M',24,'Safi',633445566,'ADA_1',1),(2,'Med','M',21,'Keni',677889900,'ADA_2',2),(3,'Test','F',23,'Toulouse',2147483647,'ADA_1',3);
-
-INSERT INTO subjects(idSub,subject_name) VALUES(1,'Math'),(2,'English');
-INSERT INTO visitor(idV,email,login_password) VALUES(1,'administrator@gmail.com','admin123'),(2,'hicham@gmail.com','hicham123'),(3,'med@gmail.com','med123'),(4,'test@gmail.com','test123');
-
-
-
-
-
-
-
+-- Create Visitor TABLE
+CREATE TABLE visitor (
+  idV int AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR (255),
+  login_password VARCHAR (255)
+);
+-- Create Administrator TABLE
+CREATE TABLE administrator (
+  idA int AUTO_INCREMENT PRIMARY KEY,
+  idV int,
+  FOREIGN KEY (idV) REFERENCES visitor(idV)
+);
+-- Create Student TABLE
+CREATE TABLE student (
+  idStu int AUTO_INCREMENT PRIMARY KEY,
+  full_name VARCHAR (255),
+  gender VARCHAR (255),
+  age int,
+  adress VARCHAR (255),
+  parent_phone int,
+  class VARCHAR (255),
+  idV int,
+  FOREIGN KEY (idV) REFERENCES visitor(idV)
+);
+-- Create Subjects TABLE
+CREATE TABLE subjects (
+  idSub int AUTO_INCREMENT PRIMARY KEY,
+  subject_name VARCHAR (255)
+);
+-- Create Marks TABLE
+CREATE TABLE marks (
+  idStu int NOT NULL,
+  idSub int NOT NULL,
+  mark FLOAT,
+  FOREIGN KEY (idStu) REFERENCES student(idStu),
+  FOREIGN KEY (idSub) REFERENCES subjects(idSub)
+);
+-- Insert data into Visitor Table
+INSERT INTO
+  visitor (email, login_password)
+values
+  ('administrator@gmail.com', 'admin123'),('hicham@gmail.com', 'hicham123'),('med@gmail.com', 'med123'),('test@gmail.com', 'test123');
+-- Insert data into Student Table
+INSERT INTO
+  student (
+    full_name,
+    gender,
+    age,
+    adress,
+    parent_phone,
+    class,
+    idV
+  )
+values
+  (
+    'Hicham',
+    'M',
+    24,
+    'Safi',
+    0633445566,
+    'ADA_1',
+    1
+  ),('Med', 'M', 21, 'Keni', 0677889900, 'ADA_2', 2),('Test', 'F', 20, 'Casa', 06123456789, 'ADA_1', 3);
+-- Insert data into Subjects Table
+INSERT INTO
+  subjects (subject_name)
+values
+  ('Math'),
+  ('English');
+-- Insert data into Marks Table
+INSERT INTO
+  marks (mark, idStu, idSub)
+values(12, 1, 1),
+  (18, 1, 2),
+  (14, 2, 1),(12, 2, 2),
+  (15, 3, 1),(12, 3, 2);
+-- Select by condition (Class ADA_1)
+SELECT
+  *
+FROM
+  student,
+  visitor
+WHERE
+  student.idV = visitor.idV
+  AND student.class = 'ADA_1';
+-- Select by condition (Hicham's mark in Math)
+select
+  student.full_name,
+  subjects.subject_name,
+  marks.mark
+FROM
+  student,
+  subjects,
+  marks
+WHERE
+  student.idStu = marks.idStu
+  and subjects.idSub = marks.idSub
+  and subjects.subject_name = "math"
+  and student.full_name = "hicham";
+-- Inner Join Selection (English marks)
+select
+  student.full_name,
+  subjects.subject_name,
+  marks.mark
+FROM
+  marks
+  INNER JOIN student ON student.idStu = marks.idStu
+  INNER JOIN subjects ON subjects.idSub = marks.idSub
+WHERE
+  subjects.subject_name = 'english';
+-- Add column to existing table
+ALTER TABLE
+  student
+ADD
+  Hobbies varchar(255);
+-- Change name & data type of column
+ALTER TABLE
+  student CHANGE Hobbies Birthday DATE;
+-- Delete column from table
+ALTER TABLE
+  student DROP COLUMN Birthday;
+-- Update existing data in a table
+UPDATE
+  student
+SET
+  age = 23,
+  adress = 'Toulouse'
+WHERE
+  full_name = "Test";
+-- Delete a row from a table
+DELETE FROM
+  `student`
+WHERE
+  `full_name` = "Test"
